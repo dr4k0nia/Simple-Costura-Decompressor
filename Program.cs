a3fa362f68a5c1ade7afba6e7f3dc526dc9c59da
Emelie Erdbeer <drakoniamacro@gmail.com>
2020-02-16 19:26:01 +0100

    Added new Formatting Restructered code New Dependencies: Colorful.Console, Costura

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 using System;
 using System.IO;
 using System.IO.Compression;
+using System.Drawing;
+using Console = Colorful.Console;
 
 namespace Costura_Decompressor
 {
@@ -9,10 +11,11 @@ namespace Costura_Decompressor
     {
         private static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
             if (args.Length == 0)
             {
-                Console.WriteLine("Usage: Costura-Decompressor file1 file2 ...");
-                Console.WriteLine("Supported File Exentsions: .exe and .compressed");
+                Console.WriteLine("Usage: Costura-Decompressor file1.exe file2.dll.compressed ...");
                 Console.ReadKey();
                 return;
             }
@@ -22,7 +25,10 @@ namespace Costura_Decompressor
                 //Check if file exists
                 string inputFile = args[i];
                 if (!File.Exists(inputFile))
-                    throw new Exception("[Error]: Invalid file: " + args[i]);
+                {
+                    WriteFormattedError("File not found", inputFile);
+                    continue;
+                }
 
                 //Check if the file is an executable
                 if (inputFile.EndsWith(".exe"))
@@ -33,10 +39,22 @@ namespace Costura_Decompressor
 
                     //Check if module has resources
                     if (!module.HasResources)
-                        throw new Exception("[Error]: No extractable resources found: " + args[i]);
+                    {
+                        WriteFormattedError("No extractable resources found", inputFile);
+                        continue;
+                    }
 
                     string outputDirectory = module.Location.Remove(module.Location.Length - module.FullName.Length) + module.Assembly.Name + @"-decompressed-resources\";
 
+                    string template = "{0} Exracting and decompressing resources from Module: {1}";
+                    Colorful.Formatter[] output = new Colorful.Formatter[]
+                    {
+                                new Colorful.Formatter("[Log]:", Color.DeepSkyBlue),
+                                new Colorful.Formatter(module.Name, Color.Cyan)
+                    };
+                    Console.WriteLineFormatted(template, Color.White, output);
+
+                    int count = 0;
                     //Get all resources
                     foreach (var resource in module.Resources)
                     {
@@ -48,37 +66,57 @@ namespace Costura_Decompressor
 
                         //Check if output file already exists
                         if (File.Exists(outputFileName))
-                            Console.WriteLine("Decompressed resource already found: " + outputFileName);
-
-                        //Create folder for the decompressed resources if it does not exist
-                        if (!Directory.Exists(outputDirectory))
-                            Directory.CreateDirectory(outputDirectory);
+                        {
+                            WriteFormattedOutput("Decompressed resource already exists", outputFileName, Color.White, Color.Yellow);
+                            count--;
+                            continue;
+                        }
 
                         else
                         {
+                            //Create folder for the decompressed resources if it does not exist
+                            if (!Directory.Exists(outputDirectory))
+                                Directory.CreateDirectory(outputDirectory);
+
                             EmbeddedResource er = module.Resources.FindEmbeddedResource(resource.Name);
                             MemoryStream bufferStream = new MemoryStream();
                             bufferStream = DecompressResource(er.CreateReader().AsStream());
                             File.WriteAllBytes(outputFileName, bufferStream.ToArray());
-                            Console.WriteLine("[Info]: Extracted and Decompressed Resource: " + resource.Name.Substring(8, resource.Name.LastIndexOf(".compressed") - 8)); // 8 = Length of "costura." which is added to the resources name by Costura
+                            Console.WriteLine(" ┕► " + resource.Name.Substring(8, resource.Name.LastIndexOf(".compressed") - 8)); // 8 = Length of "costura." which is added to the resources name by Costura
+                            count++;
                         }
                     }
-                    Console.WriteLine("[Info]: Output Directory: " + outputDirectory);
-                    Console.ReadKey();
+                    if (count == 0)
+                        WriteFormattedError("No costura embedded resources found", inputFile);
+                    else if (count > 0)
+                        WriteFormattedOutput("Extracted " + count + " resources", outputDirectory, Color.White, Color.Yellow);
                 }
+
 
                 else if (inputFile.EndsWith(".compressed"))
-                {
-                    string outputFileName = inputFile.Remove(inputFile.Length - 11); // 11 = Length of the ".compressed" extension"; I dont use Substring here because the input file is not necessarily named costura.name.extension.compressed
-                    using (FileStream bufferStream = File.OpenRead(inputFile))
-                        File.WriteAllBytes(outputFileName, DecompressResource(bufferStream).ToArray());
-                    Console.WriteLine("Decompressed file: " + args[i]);
-                }
+                    Decompress(inputFile);
 
                 else
-                    throw new Exception("[Error]: Unsupported File Extension:" + args[i]);
+                {
+                    WriteFormattedError("Unsupported file extension", inputFile);
+                    continue;
+                }
             }
             Console.ReadKey();
+        }
+
+        private static void Decompress(string inputFile)
+        {
+            string outputFileName = inputFile.Replace("costura.", null);
+            outputFileName = outputFileName.Replace(".compressed", null);
+            using (FileStream bufferStream = File.OpenRead(inputFile))
+                File.WriteAllBytes(outputFileName, DecompressResource(bufferStream).ToArray());
+            WriteFormattedOutput("Decompressed costura file", outputFileName, Color.White, Color.Yellow);
+        }
+
+        private static void ExractAndDecompress(string inputFile)
+        {
+
         }
 
         private static MemoryStream DecompressResource(Stream input)
@@ -89,6 +127,30 @@ namespace Costura_Decompressor
                 deflatestream.CopyTo(output);
             }
             return output;
+        }
+
+        private static void WriteFormattedOutput(string name, string file, Color nameColor, Color fileColor)
+        {
+            string template = "{0} {1} ► {2}";
+            Colorful.Formatter[] output = new Colorful.Formatter[]
+            {
+                                new Colorful.Formatter("[Log]:", Color.DeepSkyBlue),
+                                new Colorful.Formatter(name, nameColor),
+                                new Colorful.Formatter(file, fileColor)
+            };
+            Console.WriteLineFormatted(template, Color.White, output);
+        }
+
+        private static void WriteFormattedError(string message, string file)
+        {
+            string template = "{0} {1} ► {2}";
+            Colorful.Formatter[] output = new Colorful.Formatter[]
+            {
+                                new Colorful.Formatter("[Error]:", Color.OrangeRed),
+                                new Colorful.Formatter(message, Color.White),
+                                new Colorful.Formatter(file, Color.Yellow)
+            };
+            Console.WriteLineFormatted(template, Color.White, output);
         }
     }
 }
